57cce36745f4a96f99f687e152ce8ebc7264fd7c
KarenPNavarro <karennavarrofigueroa@protonmail.com>
2026-02-14 22:04:20 -0400

    task 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -7,22 +7,15 @@
 int main(void)
 {
 	int i;
-	int j;
 
 	i = 0;
-	while (i <= 8)
+	while (i <= 9)
 	{
-		j = i + 1;
-		while (j <= 9)
+		putchar(i + '0');
+		if (i != 9)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-			if (i != 8 || j != 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-			j++;
+			putchar(',');
+			putchar(' ');
 		}
 		i++;
 	}
