c3578970582954d3134feefa37c15e9f5bb7dc19
KarenPNavarro <karennavarrofigueroa@protonmail.com>
2026-02-21 23:41:48 -0400

    task 6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+* print_line - draws a straight line in the terminal
+* @n: the number of times the character _ should be printed
+*
+* Return: void
+*/
 
 void print_line(int n)
 {
