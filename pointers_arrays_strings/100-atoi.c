de1d9e20d115700be9aa1558bbaddc45e30add22
KarenPNavarro <karennavarrofigueroa@protonmail.com>
2026-02-22 23:44:16 -0400

    task100

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,35 +1,35 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: pointer to the string to convert
- *
- * Return: the integer value of the string
- */
+* _atoi - converts a string to an integer
+* @s: pointer to the string to convert
+*
+* Return: the integer value of the string
+*/
 int _atoi(char *s)
 {
-    int sign;
-    unsigned int result;
-    int found;
+int sign;
+unsigned int result;
+int found;
 
-    sign = 1;
-    result = 0;
-    found = 0;
+sign = 1;
+result = 0;
+found = 0;
 
-    while (*s != '\0')
-    {
-        if (*s == '-')
-            sign = sign * -1;
-        else if (*s >= '0' && *s <= '9')
-        {
-            found = 1;
-            result = result * 10 + (*s - '0');
-        }
-        else if (found == 1)
-            break;
-        s++;
-    }
-    if (sign == -1)
-        return ((int)(~result + 1));
-    return ((int)result);
+while (*s != '\0')
+{
+if (*s == '-')
+sign = sign * -1;
+else if (*s >= '0' && *s <= '9')
+{
+found = 1;
+result = result * 10 + (*s - '0');
+}
+else if (found == 1)
+break;
+s++;
+}
+if (sign == -1)
+return ((int)(~result + 1));
+return ((int)result);
 }
