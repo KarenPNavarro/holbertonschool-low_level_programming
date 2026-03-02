1e554e83e8817616b76cad6fe3d82591b107ce78
KarenPNavarro <karennavarrofigueroa@protonmail.com>
2026-03-02 09:17:46 -0400

    task 1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+* _print_rev_recursion - prints a string in reverse
+* @s: the string to be printed
+*
+* Description: Recursively prints the characters of the string in reverse order using _putchar.
+*             
+*/
+
 void _print_rev_recursion(char *s)
 {
 if (*s)
