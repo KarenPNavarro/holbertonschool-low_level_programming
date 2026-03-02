62415ba94f62ddd68c9b32cef05a46a3fa058b7f
KarenPNavarro <karennavarrofigueroa@protonmail.com>
2026-03-02 09:18:38 -0400

    task 1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -4,10 +4,9 @@
 * _print_rev_recursion - prints a string in reverse
 * @s: the string to be printed
 *
-* Description: Recursively prints the characters of the string in reverse order using _putchar.
-*             
+* Description: Recursively prints the characters of the string
+*              in reverse order using _putchar.
 */
-
 void _print_rev_recursion(char *s)
 {
 if (*s)
