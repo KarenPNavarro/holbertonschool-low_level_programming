81c031ec31e7b7026bedc03949838c3b909c0b92
KarenPNavarro <karennavarrofigueroa@protonmail.com>
2026-03-15 22:42:06 -0400

    task 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -15,32 +15,28 @@ void print_all(const char * const format, ...)
 	unsigned int i;
 	char *str;
 	char *separator;
-	char *valid;
 
 	va_start(args, format);
 
 	i = 0;
 	separator = "";
-	valid = "cifs";
 
 	while (format && format[i])
 	{
 		str = NULL;
-		if (format[i] == valid[0])
+		if (format[i] == 'c')
 			printf("%s%c", separator, va_arg(args, int));
-		else if (format[i] == valid[1])
+		else if (format[i] == 'i')
 			printf("%s%i", separator, va_arg(args, int));
-		else if (format[i] == valid[2])
+		else if (format[i] == 'f')
 			printf("%s%f", separator, va_arg(args, double));
-		else if (format[i] == valid[3])
+		else if (format[i] == 's')
 		{
 			str = va_arg(args, char *);
-			if (str == NULL)
-				str = "(nil)";
-			printf("%s%s", separator, str);
+			printf("%s%s", separator, str ? str : "(nil)");
 		}
-		if (format[i] == valid[0] || format[i] == valid[1] ||
-			format[i] == valid[2] || format[i] == valid[3])
+		if (str != NULL || format[i] == 'c' ||
+			format[i] == 'i' || format[i] == 'f')
 			separator = ", ";
 		i++;
 	}
