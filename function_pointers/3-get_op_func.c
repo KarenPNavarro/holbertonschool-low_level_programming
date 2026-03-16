3c664b0bf90afd13cb259dfcbe19302baeb0e901
KarenPNavarro <karennavarrofigueroa@protonmail.com>
2026-03-15 20:12:39 -0400

    task 3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,4 +1,5 @@
 #include "3-calc.h"
+#include <string.h>
 
 /**
  * get_op_func - selects the correct function for the operator
@@ -21,7 +22,7 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (ops[i].op != NULL)
 	{
-		if (ops[i].op[0] == s[0])
+		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
 		i++;
 	}
