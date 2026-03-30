82ffd0f1a6900c646418d29b4c8e2fad8884b03a
KarenPNavarro <karennavarrofigueroa@protonmail.com>
2026-03-30 00:02:32 -0400

    task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,53 +1,52 @@
-#include "main.h"
-#include <fcntl.h>
-#include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 /**
- * read_textfile - reads a text file and prints it to stdout
- * @filename: name of the file to read
+ * read_textfile - reads a text file and prints it to POSIX stdout
+ * @filename: the name of the file
  * @letters: number of letters to read and print
  *
- * Return: actual number of letters read and printed, or 0 on failure
+ * Return: actual number of letters read and printed, 0 on failure
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	char *buf;
-	ssize_t bytes_read;
-	ssize_t bytes_written;
+    int fd;
+    ssize_t bytes_read, bytes_written;
+    char *buffer;
 
-	if (filename == NULL)
-		return (0);
+    if (!filename || letters == 0)
+        return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        return (0);
 
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
-	{
-		close(fd);
-		return (0);
-	}
+    buffer = malloc(sizeof(char) * letters);
+    if (!buffer)
+    {
+        close(fd);
+        return (0);
+    }
 
-	bytes_read = read(fd, buf, letters);
-	if (bytes_read == -1)
-	{
-		free(buf);
-		close(fd);
-		return (0);
-	}
+    bytes_read = read(fd, buffer, letters);
+    if (bytes_read == -1)
+    {
+        free(buffer);
+        close(fd);
+        return (0);
+    }
 
-	bytes_written = write(STDOUT_FILENO, buf, bytes_read);
-	if (bytes_written != bytes_read)
-	{
-		free(buf);
-		close(fd);
-		return (0);
-	}
+    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+    if (bytes_written != bytes_read)
+    {
+        free(buffer);
+        close(fd);
+        return (0);
+    }
 
-	free(buf);
-	close(fd);
-	return (bytes_written);
+    free(buffer);
+    close(fd);
+    return (bytes_written);
 }
