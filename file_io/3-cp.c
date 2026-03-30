5930efb7767607a7c15f340886ffc3cdbe33ff3d
KarenPNavarro <karennavarrofigueroa@protonmail.com>
2026-03-29 23:57:24 -0400

    task 3

---
+++ file_io/3-cp.c
@@ -0,0 +1,78 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * close_fd - closes a file descriptor and handles error
+ * @fd: file descriptor to close
+ */
+void close_fd(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - copies content of a file to another file
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from;
+	int fd_to;
+	char buf[1024];
+	ssize_t bytes_read;
+	ssize_t bytes_written;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close_fd(fd_from);
+		exit(99);
+	}
+
+	while ((bytes_read = read(fd_from, buf, 1024)) > 0)
+	{
+		bytes_written = write(fd_to, buf, bytes_read);
+		if (bytes_written != bytes_read)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close_fd(fd_from);
+			close_fd(fd_to);
+			exit(99);
+		}
+	}
+
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close_fd(fd_from);
+		close_fd(fd_to);
+		exit(98);
+	}
+
+	close_fd(fd_from);
+	close_fd(fd_to);
+	return (0);
+}
