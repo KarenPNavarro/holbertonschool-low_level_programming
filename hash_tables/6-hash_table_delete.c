5555cd697213664943f12f2b4840c1d9cf07249b
KarenPNavarro <karennavarrofigueroa@protonmail.com>
2026-04-07 13:19:47 -0400

    task 6 delete tables

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_delete - deletes a hash table and frees all memory
+ * @ht: pointer to the hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *temp, *next;
+
+	if (!ht)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+		while (temp)
+		{
+			next = temp->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+			temp = next;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
