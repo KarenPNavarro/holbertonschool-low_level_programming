26925e43d721ad2a4e58f322a3e5fed00d191384
KarenPNavarro <karennavarrofigueroa@protonmail.com>
2026-04-07 13:15:32 -0400

    task 4

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -10,21 +10,21 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    hash_node_t *temp;
-    unsigned long int index;
+	hash_node_t *temp;
+	unsigned long int index;
 
-    if (!ht || !key || *key == '\0')
-        return (NULL);
+	if (!ht || !key || *key == '\0')
+		return (NULL);
 
-    index = key_index((const unsigned char *)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 
-    temp = ht->array[index];
-    while (temp)
-    {
-        if (strcmp(temp->key, key) == 0)
-            return (temp->value);
-        temp = temp->next;
-    }
+	temp = ht->array[index];
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
+		temp = temp->next;
+	}
 
-    return (NULL);
+	return (NULL);
 }
