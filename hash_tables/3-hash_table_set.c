31f46b9f4cdc714f23fb58afb9eee2e69d519298
KarenPNavarro <karennavarrofigueroa@protonmail.com>
2026-04-07 12:51:44 -0400

    task 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -3,10 +3,59 @@
 #include <string.h>
 
 /**
+ * create_node - creates a new hash node with key and value
+ * @key: key of the node
+ * @value: value of the node
+ *
+ * Return: pointer to new node, or NULL on failure
+ */
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+		return (NULL);
+
+	node->key = strdup(key);
+	node->value = strdup(value);
+	if (!node->key || !node->value)
+	{
+		free(node->key);
+		free(node->value);
+		free(node);
+		return (NULL);
+	}
+
+	node->next = NULL;
+	return (node);
+}
+
+/**
+ * update_node - updates the value of an existing node
+ * @node: node to update
+ * @value: new value
+ *
+ * Return: 1 on success, 0 on failure
+ */
+int update_node(hash_node_t *node, const char *value)
+{
+	char *new_value;
+
+	new_value = strdup(value);
+	if (!new_value)
+		return (0);
+
+	free(node->value);
+	node->value = new_value;
+	return (1);
+}
+
+/**
  * hash_table_set - adds or updates an element in a hash table
- * @ht: pointer to the hash table
+ * @ht: pointer to hash table
  * @key: key to add (cannot be empty)
- * @value: value associated with the key (must be duplicated)
+ * @value: value associated with key (duplicated)
  *
  * Return: 1 on success, 0 on failure
  */
@@ -18,45 +67,20 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!ht || !key || *key == '\0' || !value)
 		return (0);
 
-	/* Get index using hash function */
 	index = key_index((const unsigned char *)key, ht->size);
 
-	/* Check if key already exists; if so, update */
 	temp = ht->array[index];
 	while (temp)
 	{
 		if (strcmp(temp->key, key) == 0)
-		{
-			free(temp->value);
-			temp->value = strdup(value);
-			if (!temp->value)
-				return (0);
-			return (1);
-		}
+			return (update_node(temp, value));
 		temp = temp->next;
 	}
 
-	/* Key not found, create new node */
-	new_node = malloc(sizeof(hash_node_t));
+	new_node = create_node(key, value);
 	if (!new_node)
 		return (0);
 
-	new_node->key = strdup(key);
-	if (!new_node->key)
-	{
-		free(new_node);
-		return (0);
-	}
-
-	new_node->value = strdup(value);
-	if (!new_node->value)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
-	}
-
-	/* Insert at beginning of list */
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
